1d0a91dc61602bbc76e152e105388cf052e3d67f
Tomas Dvorak <todvora@gmail.com>
2015-08-24 20:13:25 +0200

    styles, better wsdl type check

--- data/readpage.js
+++ data/readpage.js
@@ -1,6 +1,7 @@
 // Reads the XML data and forwards them back to the index.js script.
 
-if(document.firstChild.tagName == 'definitions') {
+var rootTag = document.firstChild.tagName;
+if(typeof rootTag !== 'undefined' && rootTag.indexOf('definitions') !== -1) {
   // the page seems to contain WSDL definition.
   payload = self.postMessage((new XMLSerializer()).serializeToString(document));
 } else {
